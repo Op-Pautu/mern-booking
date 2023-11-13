70ebff378ed75c77e8e0fa52a9d41deff9b2639e
Op-Pautu <pautuop@gmail.com>
2023-11-13 19:23:37 +0530

    add api endpoint for login

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const User = require("./models/User");
 
@@ -18,7 +19,9 @@ app.use(
 mongoose.connect(process.env.MONGO_URL);
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = process.env.JWT_SECRET;
 
+console.log(jwtSecret);
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
@@ -36,6 +39,32 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch (e) {
     res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const userDoc = await User.findOne({ email });
+
+  if (!userDoc) {
+    res.json("not found");
+  }
+
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (passOk) {
+    jwt.sign(
+      { email: userDoc.email, id: userDoc._id },
+      jwtSecret,
+      { expiresIn: "24h" },
+      (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).json("pass ok");
+      }
+    );
+  } else {
+    res.status(422).json("Wrong credentials");
   }
 });
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,45 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/login", {
+        email,
+        password,
+      });
+      setRedirect(true);
+      alert("Login Successful. ");
+    } catch (error) {
+      alert("Login failed. Please try again");
+    }
+  };
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
   return (
     <div className="mt-4 flex grow items-center justify-around">
       <div className="-mt-64">
         <h1 className="mb-4 text-center text-4xl">Login</h1>
-        <form className="mx-auto max-w-md">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="mx-auto max-w-md" onSubmit={loginUser}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="primary">Login</button>
           <div className="py-2 text-center text-gray-500">
             Don&apos;t have an account yet?{" "}
